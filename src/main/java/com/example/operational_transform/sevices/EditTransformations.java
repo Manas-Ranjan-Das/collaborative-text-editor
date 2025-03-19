69f8ae7930fdc1645a41eb3e24268e50aba3ed63
Manas Ranjan Das <manasranjandas.student@gmail.com>
2025-03-20 04:02:52 +0530

    Temp commit

--- src/main/java/com/example/operational_transform/sevices/EditTransformations.java
+++ src/main/java/com/example/operational_transform/sevices/EditTransformations.java
@@ -1,13 +1,25 @@
 package com.example.operational_transform.sevices;
 
+import com.example.operational_transform.dto.ChangeDTO;
 import com.example.operational_transform.dto.EditDTO;
+import com.example.operational_transform.dto.RangeDTO;
 
 public class EditTransformations {
     
 
     EditDTO transformBasedOn(EditDTO editToTranform , EditDTO transformBasedOn){
-        
 
+        ChangeDTO change = transformBasedOn.getChanges().get(0);
+        RangeDTO range = change.getRange();
+        int numberOfLinesRemoved = range.getEndLineNumber()  -  range.getStartLineNumber();
+        int numberOfExtraCharsRemoved = range.getEndColumn();
+
+        String[] addedLines = change.getText().split("\n") ;
+        int numberOfLinesAdded = addedLines.length - 1;
+        int numberOfExtraCharsAdded = addedLines[addedLines.length-1].length();
+
+
+        // TODO : 
 
 
         return editToTranform;
